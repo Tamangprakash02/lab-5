9c264a9f44dbfbda87a9f8cbe3d49022c82b2863
prakashtamang <tamangprakash02@gmail.com>
2023-01-04 09:28:09 +0545

    Challenge: another way of adding whitespaces to make code more readable

--- program5.cs
+++ program5.cs
@@ -1,18 +1,25 @@
-string str = "The quick brown fox jumps over the lazy dog.";
-// convert the message into a char array
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
 
-char[] charMessage = str.ToCharArray();
-// Reverse the chars
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
-Array.Reverse(charMessage);
-int x = 0;
-// count the o's
-foreach (char i in charMessage) 
-{ if (i == 'o') { x++; } }
-// convert it back to a string
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
 
-string new_message = new String(charMessage);
-// print it out
+int letterCount = 0;
 
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
