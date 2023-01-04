1bcdd8197870ca50ecf32bb7968022db906a72da
prakashtamang <tamangprakash02@gmail.com>
2023-01-04 09:26:55 +0545

    Challenge: Adding whitespaces to make code more readable

--- program5.cs
+++ program5.cs
@@ -1,18 +1,18 @@
-Random dice = new Random();
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
 
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-    if ((roll1 == roll2) && (roll2 == roll3)) {
-        Console.WriteLine("You rolled triples!  +10 bonus to total!");
-        total += 10; 
-    } else {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
-}
-//added whitespaces to make the code more readable
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) 
+{ if (i == 'o') { x++; } }
+// convert it back to a string
+
+string new_message = new String(charMessage);
+// print it out
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
